43d9404f4cae9142688542d0b2d20d23320efe65
applicature-dlitvinov <101503007+applicature-dlitvinov@users.noreply.github.com>
2022-08-03 18:24:59 +0300

    fix(UC-68): fix responcive account modal (#55)

---
+++ projects/applicature/ngx-web3-synergy/src/lib/directives/check-el-width/check-el-width.directive.ts
@@ -0,0 +1,50 @@
+import { AfterViewInit, Directive, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, filter, takeUntil } from 'rxjs/operators';
+
+import { BaseSubscriber } from '../../helpers';
+
+
+@Directive({
+  selector: '[w3sCheckElWidth]',
+  exportAs: 'w3sCheckElWidth'
+})
+export class W3sCheckElWidthDirective extends BaseSubscriber implements AfterViewInit {
+  @Output() elWidth: EventEmitter<number> = new EventEmitter<number>();
+  private _resize$: Subject<number> = new Subject();
+  private _curElWidth = 0;
+
+  private get _elRefWidth(): number {
+    return this._elementRef.nativeElement.clientWidth ?? 0;
+  }
+
+  @HostListener(`window:resize`)
+  onResize() {
+    this._resize$.next(this._elRefWidth);
+  }
+
+  constructor(private _elementRef: ElementRef) {
+    super();
+
+    this._resize$
+      .pipe(
+        debounceTime(50),
+        filter((width: number) => width !== this._curElWidth),
+        takeUntil(this.notifier)
+      )
+      .subscribe(() => {
+        this._emitWidth();
+      });
+
+  }
+
+  ngAfterViewInit(): void {
+    this._emitWidth();
+  }
+
+  private _emitWidth(): void {
+    this._curElWidth = this._elRefWidth;
+    this.elWidth.emit(this._curElWidth);
+  }
+
+}
---
+++ projects/applicature/ngx-web3-synergy/src/lib/directives/check-el-width/index.ts
@@ -0,0 +1,1 @@
+export * from './check-el-width.directive';
