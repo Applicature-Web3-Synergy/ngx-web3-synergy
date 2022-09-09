329f88f4ada4026bdeb8453d1ec64abb478d67af
SG <truespawner@gmail.com>
2022-09-09 11:12:55 +0300

    feat(UC-82): Redesign sidebar

---
+++ projects/ngx-web3-synergy-doc/src/app/constants/icon-titles-by-url.constant.ts
@@ -0,0 +1,13 @@
+import { ROUTER_LINKS } from '../enums';
+
+export const IconTitlesByUrl = {
+  [ROUTER_LINKS.GETTING_STARTED]: 'assets/img/sidebar/transaction-history.svg',
+  [ROUTER_LINKS.CONNECT_WALLET]: 'assets/img/sidebar/wallet-connect.svg',
+  [ROUTER_LINKS.ACCOUNT_BALANCE]: 'assets/img/sidebar/account-balance.svg',
+  [ROUTER_LINKS.ACCOUNT_BUTTON]: 'assets/img/sidebar/account-btn.svg',
+  [ROUTER_LINKS.TRANSACTION_HISTORY]: 'assets/img/sidebar/transaction-history.svg',
+  [ROUTER_LINKS.TABLE]: 'assets/img/sidebar/table.svg',
+  [ROUTER_LINKS.DIALOG]: 'assets/img/sidebar/dialog.svg',
+  [ROUTER_LINKS.NETWORK_DROPDOWN]: 'assets/img/sidebar/network-dropdown.svg',
+  [ROUTER_LINKS.FAUCET]: 'assets/img/sidebar/network-dropdown.svg',
+}
