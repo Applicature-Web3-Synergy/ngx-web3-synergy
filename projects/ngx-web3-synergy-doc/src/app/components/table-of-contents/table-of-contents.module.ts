5218b754c12fcec28eeeee3fdb4f22aac109aa43
SG <truespawner@gmail.com>
2022-09-15 22:03:51 +0300

    feat(UC-89): Fix import

--- projects/ngx-web3-synergy-doc/src/app/components/table-of-contents/table-of-contents.module.ts
+++ projects/ngx-web3-synergy-doc/src/app/components/table-of-contents/table-of-contents.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TableOfContentComponent } from './table-of-contents.component';
 
+import { TableOfContentComponent } from './table-of-contents.component';
 
 
 @NgModule({
